dd6d2c36f7d6c523339853fd0575794b19b27f5f
Aaron <c12444752@mydit.ie>
2018-07-31 21:10:34 +0100

    removing old test files

--- src/main/java/com/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import com.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Test,Long> {
-}
